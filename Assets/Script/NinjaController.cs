2278f189c1364f39769576c4644bae0efcb186ec
AlexisJES <alexis_27_2@hotmail.com>
2023-04-10 00:37:01 -0500

    Animaciones arregladas

--- Assets/Script/NinjaController.cs
+++ Assets/Script/NinjaController.cs
@@ -53,6 +53,16 @@ public class NinjaController : MonoBehaviour
             currentAnimation = 4;
         }
 
+        if (Input.GetKey(KeyCode.Z))
+        {
+            currentAnimation = 5;
+        }
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            currentAnimation = 6;
+        }
+
         anim.SetInteger("Ninja", currentAnimation);
 
     }
